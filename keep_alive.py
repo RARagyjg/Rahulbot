c3a071963348040804150d39b7ca000a2050c186
RARagyjg <140886373+RARagyjg@users.noreply.github.com>
2025-07-06 14:42:03 +0530

    keep_alive.py

--- keep_alive.py
+++ keep_alive.py
@@ -1,3 +1,4 @@
+# keep_alive.py
 from flask import Flask
 from threading import Thread
 
@@ -5,11 +6,7 @@ app = Flask('')
 
 @app.route('/')
 def home():
-    return "✅ I am alive bro!"
-
-def run():
-    app.run(host='0.0.0.0', port=8080)
+    return "✅ LIVE HU GANDU"
 
 def keep_alive():
-    t = Thread(target=run)
-    t.start()
+    Thread(target=lambda: app.run(host='0.0.0.0', port=8080)).start()
